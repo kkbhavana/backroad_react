1658c63ee0fc84449fb47c11c08d02e602fbb5f8
kkbhavana <kkbhavana6@gmail.com>
2024-05-19 12:24:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import About from "./components/About";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About/>
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
